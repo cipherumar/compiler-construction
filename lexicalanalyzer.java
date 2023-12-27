c92053f13f640bbd224aa3f8a8474f827d765d4e
cipherumar <umarfarooq.cyber@gmail.com>
2023-12-27 21:15:19 +0500

    first code

---
+++ lexicalanalyzer.java
@@ -0,0 +1,30 @@
+public class CommentRemover {
+    public static String removeComments(String code) {
+        // Remove single-line comments
+        code = code.replaceAll("(?<!['\"\\\\])//.*", "");
+
+        // Remove multi-line comments
+        code = code.replaceAll("/\\*(.|[\\n\\r])*?\\*/", "");
+
+        return code;
+    }
+
+    public static void main(String[] args) {
+        // Example code with comments
+        String codeWithComments = "public class HelloWorld {\n" +
+                "    // This is a single-line comment\n" +
+                "    public static void main(String[] args) {\n" +
+                "        /*\n" +
+                "         * This is a multi-line comment\n" +
+                "         */\n" +
+                "        System.out.println(\"Hello, World!\");\n" +
+                "    }\n" +
+                "}\n";
+
+        // Remove comments
+        String codeWithoutComments = removeComments(codeWithComments);
+
+        // Print the code without comments
+        System.out.println(codeWithoutComments);
+    }
+}
